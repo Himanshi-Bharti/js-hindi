5d17f7b02d082e6dd3677076fa5121d349e11439
himanshi-bharti <115403919+Himanshi-Bharti@users.noreply.github.com>
2025-11-05 14:01:11 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";     // treat all js code as newer version of js.
+
+//  alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log( 3 + 3);
+
+
+/* 
+
+number => 2 to pwr 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value means it has empty value 
+undefined => undefined means not defined
+symbol => unique
+
+object 
+
+*/
+console.log(typeof null);         // output is object
+console.log(typeof undefined);    // output is undefined
+console.log(typeof "himanshi");   // output is string
+
+
+
