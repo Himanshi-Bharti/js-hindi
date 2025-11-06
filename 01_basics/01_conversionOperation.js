466902aba3d9049bde77743f188970bce2e1c761
himanshi-bharti <115403919+Himanshi-Bharti@users.noreply.github.com>
2025-11-06 03:00:59 +0000

    operations on datatypes

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,3 +1,6 @@
+// *****************************************************conversion**************************************************************
+
+
 let score = 33                            // datatype is number
 
 let score1 = "33"                         // datatype is string coz in ""
@@ -30,3 +33,64 @@ let booleanisNum = Boolean(isNum)
 console.log(booleanisNum);
 
 // so if the isNum has a value in it either 1 or 0 or "himanshi" then the output will be true and if its empty like "", null, undefined then the value will be false
+
+
+
+
+
+
+// *****************************************************operation**************************************************************
+
+
+let num = 3
+let negNum = -num                      // assigning negative number to negNum just use - infront of the variable 
+console.log(negNum);                   // output -3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);                     // 2 raise tot he power 3
+// console.log(2/3);
+// console.log(2%3);                      // remainder 2/3
+
+
+let str1 = "Hello beautiful"
+let str2 = " Himanshi baby.."
+let str3 = console.log(str1+str2);        // concatination
+
+console.log("1" + 2);                     // 12
+console.log(1 + "2");                     // 12
+console.log("1" + "2");                   // 12
+
+console.log("1" + 2 + 2);                 // 122
+console.log(1 + 2 + "2");                 // 32 
+console.log(1 + "2" + 2);                 // 122
+
+console.log(+true);                       // 1
+// console.log(true+);                       // error
+console.log(+"");                         // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+3
+
+let gameCounter = 1
+let gameCounter1 = 1
+// ++gameCounter;
+// gameCounter1++
+
+// console.log(gameCounter);
+console.log((gameCounter++) + (++gameCounter1));
+console.log(gameCounter);
+
+console.log(++gameCounter1);
+
+
+// link to study tc39.es/ecma262 sec-type-conversion
+
+
+
+
+
+
